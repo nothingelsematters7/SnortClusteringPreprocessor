1210b81dc6f3a741d94a16bf074517eb3576ee17
user <user@ubuntu.(none)>
2013-04-02 07:37:07 -0400

    converted tabs to spaces

--- cluster/cluster.py
+++ cluster/cluster.py
@@ -9,13 +9,13 @@ NCLUSTERS = 3
 data = []
 
 with open('output.csv', 'rUb') as csvfile:
-	reader = csv.reader(csvfile, delimiter=';')
-	for row in reader:
-		if reader.line_num == 1:
-			continue
-		# if reader.line_num == 10000:
-		# 	break
-		data.append([int(item) for item in row])
+    reader = csv.reader(csvfile, delimiter=';')
+    for row in reader:
+        if reader.line_num == 1:
+            continue
+        # if reader.line_num == 10000:
+        #   break
+        data.append([int(item) for item in row])
 
 print data[0]
 labels = ['N' if row[-1] == 0 else 'A' for row in data]
@@ -25,22 +25,22 @@ std = [0] * len(data[0])
 
 
 for row in data:
-	for i, item in enumerate(row):
-		average[i] += item
+    for i, item in enumerate(row):
+        average[i] += item
 
 average = [float(i) / len(data) for i in average]
 
 for row in data:
-	for i, item in enumerate(row):
-		std[i] += (item - average[i]) ** 2
+    for i, item in enumerate(row):
+        std[i] += (item - average[i]) ** 2
 
 std = [sqrt(float(i) / (len(data) - 1)) for i in std]
 print average
 print std
 
 for i in xrange(len(data)):
-	for j in xrange(len(data[0])):
-		data[i][j] = (data[i][j] - average[j]) / std[j]
+    for j in xrange(len(data[0])):
+        data[i][j] = (data[i][j] - average[j]) / std[j]
 
 print data[0]
 
@@ -50,16 +50,16 @@ print clusterid, error, nfound
 
 # output_file = open('output.log', 'w')
 # for i in xrange(len(labels)):
-# 	output_file.write(labels[i] + ' ' + str(clusterid[i]))
+#   output_file.write(labels[i] + ' ' + str(clusterid[i]))
 
 attacks_per_cluster = [0] * NCLUSTERS
 normal_per_cluster = [0] * NCLUSTERS
 
 for i in xrange(len(labels)):
-	if labels[i] == 'A':
-		attacks_per_cluster[clusterid[i]] += 1
-	else:
-		normal_per_cluster[clusterid[i]] += 1
+    if labels[i] == 'A':
+        attacks_per_cluster[clusterid[i]] += 1
+    else:
+        normal_per_cluster[clusterid[i]] += 1
 
 print "Attacks"
 print ['%4d' % (item,) for item in attacks_per_cluster]
